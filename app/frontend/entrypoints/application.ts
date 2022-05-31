211e345af28b398049d961862cc9abc1b0460fd7
reyolan <reynardolan@gmail.com>
2022-05-31 18:50:55 +0800

    first commit

---
+++ app/frontend/entrypoints/application.ts
@@ -0,0 +1,19 @@
+import "../../assets/stylesheets/application.css";
+
+console.log("Vite ⚡️ Rails");
+
+console.log(
+  "Visit the guide for more information: ",
+  "https://vite-ruby.netlify.app/guide/rails"
+);
+
+// Example: Load Rails libraries in Vite.
+//
+// import * as Turbo from '@hotwired/turbo'
+// Turbo.start()
+//
+// import ActiveStorage from '@rails/activestorage'
+// ActiveStorage.start()
+//
+// // Import all channels.
+// const channels = import.meta.globEager('./**/*_channel.js')
