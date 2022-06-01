4abbe93a7691e38481b2525f7ae20421f67ff951
reyolan <reynardolan@gmail.com>
2022-06-01 19:57:01 +0800

    add and edit layout

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,7 +1,7 @@
 class Color < ApplicationRecord
   validates :name, uniqueness: true, length: { maximum: 55 }, presence: true
-  validates :description, length: { maximum: 240 }, presence: true
+  validates :description, length: { maximum: 100 }, presence: true
   validates :hex, length: { is: 6 }, presence: true
-  validates :rgb, presence: true, length: { minimum: 7, maximum: 13 }
-  validates :hsl, presence: true, length: { minimum: 9, maximum: 17 }
+  validates :rgb, length: { minimum: 7, maximum: 13 }, presence: true
+  validates :hsl, length: { minimum: 9, maximum: 17 }, presence: true
 end
