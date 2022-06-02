4bf2e3f094fe513752c806615af09850ee51484f
reyolan <reynardolan@gmail.com>
2022-06-03 02:35:39 +0800

    update model-level validation

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,6 +1,6 @@
 class Color < ApplicationRecord
-  validates :name, uniqueness: true, length: { maximum: 55 }, presence: true
-  validates :description, length: { maximum: 100 }, presence: true
+  validates :name, uniqueness: true, length: { maximum: 16 }, presence: true
+  validates :description, length: { maximum: 145 }, presence: true
   validates :hex, length: { is: 6 }, presence: true
   validates :rgb, length: { minimum: 7, maximum: 13 }, presence: true
   validates :hsl, length: { minimum: 9, maximum: 17 }, presence: true
