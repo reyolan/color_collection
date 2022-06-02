b77461b263ae397cca4d6a7d0acc85ac9dfece85
reyolan <reynardolan@gmail.com>
2022-06-03 04:06:47 +0800

    add mobile layout

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,5 +1,5 @@
 class Color < ApplicationRecord
-  validates :name, uniqueness: true, length: { maximum: 16 }, presence: true
+  validates :name, uniqueness: true, length: { maximum: 15 }, presence: true
   validates :description, length: { maximum: 145 }, presence: true
   validates :hex, length: { is: 6 }, presence: true
   validates :rgb, length: { minimum: 7, maximum: 13 }, presence: true
