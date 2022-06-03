040379b0510651a8ac8b9e4435abe86b2db65635
reyolan <reynardolan@gmail.com>
2022-06-04 05:34:48 +0800

    add regex validation

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,7 +1,7 @@
 class Color < ApplicationRecord
   validates :name, uniqueness: true, length: { maximum: 15 }, presence: true
   validates :description, length: { maximum: 145 }, presence: true
-  validates :hex, length: { is: 6 }, presence: true
-  validates :rgb, length: { minimum: 7, maximum: 13 }, presence: true
-  validates :hsl, length: { minimum: 9, maximum: 17 }, presence: true
+  validates :hex, format: { with: /\A([A-Fa-f0-9]{6})\z/ }, presence: true
+  validates :rgb, format: { with: /\A(\d{1,3}), (\d{1,3}), (\d{1,3})\z/ }, presence: true
+  validates :hsl, format: { with: /\A(\d{1,3}), (\d{1,3}(.\d{1})?%), (\d{1,3}(.\d{1})?%)\z/ }, presence: true
 end
